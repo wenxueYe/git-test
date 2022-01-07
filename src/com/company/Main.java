03b1415e9fe55920d0e316694fd1ec96ea41d934
yewenxue <yewenxuewy@163.com>
2022-01-07 22:07:12 +0800

    在IDEA上测试文件从暂存区到本地库

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello,this is yewenxue.");
+        System.out.println("===");
+        System.out.println("new Main");
+    }
+}
