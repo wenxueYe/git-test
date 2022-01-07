69134a603b08211d82c57c72d69dbac3b30a3bda
yewenxue <yewenxuewy@163.com>
2022-01-07 22:17:11 +0800

    第二次提交，这次应该被忽略的差不多了

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello,this is yewenxue.");
-        System.out.println("===");
+
+
         System.out.println("new Main");
     }
 }
